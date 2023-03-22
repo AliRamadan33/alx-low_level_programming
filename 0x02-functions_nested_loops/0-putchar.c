791945fd25f867180c17a7eda7e5489dd4ee9e25
LinCu0 <alitoba335@gmail.com>
2023-03-22 06:52:31 -0700

    af

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,19 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main() {
-    printf("_putchar\n");
-    return 0;
+int main(void)
+{
+	char *sh = "Holberton";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
 }
