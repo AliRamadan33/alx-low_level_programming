22b209065e531dcdc1f65c8065fe128a50971868
LinCu0 <alitoba335@gmail.com>
2023-03-22 07:42:06 -0700

    af

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,9 +5,9 @@
  * @c: single letter input
  * Return: 1 if int c is lowercase, 0 if otherwise
  */
-int _islower(int num)
+int _islower(int c)
 {
-	if (num >= 'a' && num <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
