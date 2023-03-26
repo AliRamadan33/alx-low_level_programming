7928a89c0bcea0e3151cba97be8589d267939c48
LinCu0 <alitoba335@gmail.com>
2023-03-26 07:23:59 -0700

    LoL

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - function that checks for uppercase character
+ * @c: is input for this function
+ * Return: returns 1 if c is upper Returns 0 otherwise
+ */
+int _isdigit(int c) 
+{
+if (c >= 0 && c <= 9)
+return (1);
+
+else
+
+return (0);
+
+}
