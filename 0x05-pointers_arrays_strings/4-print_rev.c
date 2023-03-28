8787690f1d9ac15579209d6fc62ce6096857d5fd
LinCu0 <alitoba335@gmail.com>
2023-03-28 07:27:12 -0700

    afas

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+    if (s == NULL)
+        return;
+
+    int len = 0;
+    while (s[len] != '\0')
+        len++;
+
+    for (int i = len - 1; i >= 0; i--)
+        putchar(s[i]);
+
+    putchar('\n');
+}
