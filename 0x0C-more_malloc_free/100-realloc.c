ece175559f61b841aaf3a2cf41e6ce97c980840e
LinCu0 <alitoba335@gmail.com>
2023-04-14 16:19:14 -0700

    KK

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,19 +11,8 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
-
-	if (new_size == old_size)
-		return (ptr);
-
-	if (ptr == NULL)
-	{
-		new_ptr = malloc(new_size);
-		if (new_ptr == NULL)
-			return (NULL);
-		free(ptr);
-		return (new_ptr);
-	}
+	void *new_p;
+	unsigned int i;
 
 	if (new_size == 0 && ptr != NULL)
 	{
@@ -31,16 +20,21 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
-	new_ptr = malloc(new_size);
-	if (new_ptr == NULL)
+	if (ptr == NULL)
+		return (malloc(new_size));
+
+	if (new_size <= old_size)
+		return (ptr);
+
+	new_p = malloc(new_size);
+
+	if (new_p == NULL)
 		return (NULL);
 
-	if (new_size > old_size)
-		new_size = old_size;
-
-	new_ptr = _memcpy(new_ptr, ptr, new_size);
+	for (i = 0; i < old_size && i < new_size; i++)
+		*((char *)new_p + i) = *((char *)ptr + i);
 
 	free(ptr);
 
-	return (new_ptr);
+	return (new_p);
 }
