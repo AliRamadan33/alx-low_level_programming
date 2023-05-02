5bf767f926aaee9936e611a2f38a6dffaf32b0cf
LinCu0 <git config --global user.email>
2023-05-02 13:47:18 -0700

    More

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,12 +8,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t count = 0;
+	size_t cont = 0;
+
 	while (h)
 	{
-		count++;
+		cont++;
 		h = h->next;
 	}
-	
-	return (count);
+
+	return (cont);
 }
