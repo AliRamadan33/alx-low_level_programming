edd93d0dd97ca90e926a2383bf8238d9f5d7f5fd
LinCu0 <git config --global user.email>
2023-05-02 13:36:47 -0700

    More

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,20 +2,21 @@
 
 /**
  * print_listint - Prints all the elements of a listint_t list.
- * @h: linked list of type listint_t to print
+ * @h: A pointer to the head of the listint_t list.
  *
- * Return: number of nodes
+ * Return: The number of nodes in the listint_t list.
  */
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
-	
+
 	while (h)
 	{
 		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
-	
+
 	return (count);
 }
+
