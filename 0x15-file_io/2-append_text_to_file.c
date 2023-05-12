b4d4d33ded8e8b5d708e806f2afaf3ec5907d089
LinCu0 <alitoba335@gmail.com>
2023-05-12 06:00:11 -0700

    I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,7 +10,9 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int FD,Nletters,RWR;
+	int FD;
+	int nletters;
+	int RWR;
 
 	if (!filename)
 		return (-1);
@@ -22,10 +24,10 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (Nletters = 0; text_content[Nletters]; Nletters++)
+		for (nletters = 0; text_content[nletters]; nletters++)
 			;
 
-		RWR = write(fd, text_content, Nletters);
+		RWR = write(fd, text_content, nletters);
 
 		if (RWR == -1)
 			return (-1);
